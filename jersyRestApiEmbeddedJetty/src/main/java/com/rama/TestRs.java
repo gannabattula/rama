3a245f4d650ef9908815be55af461056a07cb399
Rama <rgannabattula@galtsoft.in>
2016-04-28 22:01:24 +0530

    comit

---
+++ jersyRestApiEmbeddedJetty/src/main/java/com/rama/TestRs.java
@@ -0,0 +1,35 @@
+package com.rama;
+
+import java.awt.print.Book;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@Path("/test")
+public class TestRs {
+	
+	
+	
+    @GET
+    //@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public BookType getBookInfo() {
+ 
+        // retrieve book information based on the id supplied in the formal argument
+ 
+        BookType bookType = new BookType();
+        bookType.setBookId(2522);
+        bookType.setBookName("rrrrr");
+        bookType.setAuthor("");
+        bookType.setCategory("sss");
+        return bookType;
+    }
+
+}
---
+++ jersySpringSqliteRestApi/src/main/java/com/rama/App.java
@@ -0,0 +1,8 @@
+package com.rama;
+
+public class App {
+
+	//https://nikolaygrozev.wordpress.com/2014/10/16/rest-with-embedded-jetty-and-jersey-in-a-single-jar-step-by-step/
+	//https://www.jamesward.com/2012/08/13/containerless-spring-mvc
+	//http://digitalsanctum.com/2012/11/24/how-to-embed-jetty-with-spring-mvc/
+}
